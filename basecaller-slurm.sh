0863667ce1c843a9344c626a4a0f89d876e049b2
Jodie <jomojaco@ucsc.edu>
2025-08-29 09:09:26 -0700

    snakemake

--- basecaller-slurm.sh
+++ basecaller-slurm.sh
@@ -11,20 +11,27 @@
 #SBATCH --time=06:00:00
 
 set -o pipefail
-# cause a bash script to exit immediately when a command fails
 set -e
-# cause the bash shell to treat unset variables as an error and exit immediately
 set -u
-# echo each line of the script to stdout so we can see what is happening
-# to turn off echo do 'set +o xtrace'
 set -o xtrace
 
 POD5DIR=$1
 OUTDIR=$2
 REF=$3
-#/private/home/jomojaco/dorado-0.7.3-linux-x64/bin/dorado basecaller hac $POD5DIR  --device cuda:all --output-dir $OUTDIR
-#/private/home/jomojaco/dorado-0.7.3-linux-x64/bin/dorado basecaller demux $OUTDIR -o $OUTDIR
 
-/private/home/jomojaco/dorado-0.7.3-linux-x64/bin/dorado basecaller hac $POD5DIR  --device cuda:all --reference $REF --kit-name SQK-NBD114-24 > $OUTDIR"/aligned_basecalled.bam" 
-/private/home/jomojaco/dorado-0.7.3-linux-x64/bin/dorado demux $OUTDIR -o $OUTDIR"/demux" --kit-name SQK-NBD114-24 
- 
+# Ensure output directory exists
+mkdir -p "$OUTDIR"
+mkdir -p "$OUTDIR/demux"
+
+# Basecalling with alignment
+/private/home/jomojaco/dorado-0.7.3-linux-x64/bin/dorado basecaller hac $POD5DIR \
+    --device cuda:all \
+    --reference $REF \
+    --kit-name SQK-NBD114-24 \
+    > $OUTDIR/aligned_basecalled.bam
+
+# Demux the specific BAM file
+/private/home/jomojaco/dorado-0.7.3-linux-x64/bin/dorado demux \
+    "$OUTDIR/aligned_basecalled.bam" \
+    --output-dir "$OUTDIR/demux" \
+    --kit-name SQK-NBD114-24
